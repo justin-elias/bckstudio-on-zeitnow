3f5ab1b0c020ae955527a698c9e973076448f116
justinelias <stryder03@pm.me>
2024-12-17 19:43:14 -0700

    feat(MembershipPage): Added ability to disable purchasing a membership

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -6,106 +6,140 @@ import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
 import StarIcon from "@material-ui/icons/StarBorder";
 import Typography from "@material-ui/core/Typography";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Divider from "@material-ui/core/Divider";
 import classNames from "classnames";
 import GridContainer from "../Grid/GridContainer";
 import GridItem from "../Grid/GridItem";
 
 const useStyles = makeStyles((theme) => ({
-    "@global": {
-        ul: {
-            margin: 0,
-            padding: 0,
-            listStyle: "none",
-        },
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
     },
-    link: {
-        margin: theme.spacing(1, 1.5),
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  cardHeader: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+  cardPricing: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2),
+  },
+  cards: {
+    marginTop: "2.5rem",
+  },
+  priceButton: {
+    fontWeight: "bold",
+  },
+  featuredPriceButton: {
+    fontWeight: "bold",
+    "&:hover": {
+      color: "#ffffff",
     },
-    cardHeader: {
-        backgroundColor: theme.palette.secondary.main
-    },
-    cardPricing: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "baseline",
-        marginBottom: theme.spacing(2),
-    },
-    cards: {
-        marginTop: "2.5rem",
-    },
-    priceButton: {
-        fontWeight: "bold",
-    },
-    featuredPriceButton: {
-        fontWeight: "bold",
-        "&:hover": {
-            color: "#ffffff"
-        }
-    },
-    categoryHeaders: {
-        color: theme.palette.primary.main,
-        marginTop: "2.5rem"
-    },
-    smButton: {
-        maxWidth: "50%"
-    },
-
+  },
+  categoryHeaders: {
+    color: theme.palette.primary.main,
+    marginTop: "2.5rem",
+  },
+  smButton: {
+    maxWidth: "50%",
+  },
 }));
 
 export default function Pricing(props) {
-    const classes = useStyles();
+  const classes = useStyles();
+  const { tierCategory, title, maxWidth } = props;
 
-    const {tierCategory, title, maxWidth} = props;
-
-    return (<React.Fragment>
-            <Typography variant={"h2"} align={"center"} className={classes.categoryHeaders}>
-                {title}
-            </Typography>
-            <GridContainer spacing={5} alignItems={"flex-end"} justifyContent={"center"}>
-            {tierCategory.membershipTiers.map((tier) => (
-                        <GridItem key={tier.title} xs={12} sm={12} md={4}>
-                            <Card className={classes.cards}>
-                                <CardHeader
-                                    title={tier.title}
-                                    titleTypographyProps={{ align: "center" }}
-                                    subheaderTypographyProps={{ align: "center" }}
-                                    action={tier.title === "Standard" ? <StarIcon /> : null}
-                                    className={classes.cardHeader}
-                                />
-                                <CardContent>
-                                    <div className={classes.cardPricing}>
-                                        <Typography component="h2" variant="h3" color="textPrimary">
-                                            ${tier.price}
-                                        </Typography>
-                                        <Typography variant="h6" color="textSecondary">
-                                            /{tier.term}
-                                        </Typography>
-                                    </div>
-                                    <ul>
-                                        {tier.description.map((line) => (
-                                            <Typography component="li" variant="subtitle1" align="left" key={line}>
-                                                <Divider/>
-                                                {line}
-                                                <Divider/>
-                                            </Typography>
-                                        ))}
-                                    </ul>
-                                </CardContent>
-                                <CardActions>
-                                    <Button fullWidth={maxWidth !== "sm"}
-                                            variant={tier.buttonVariant}
-                                            color="primary"
-                                            href={tier.inputValue}
-                                            className={classNames(
-                                                tier.buttonVariant === "contained" ? classes.featuredPriceButton : classes.priceButton)}>
-                                        {tier.buttonText}
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        </GridItem>
-            ))}
-            </GridContainer>
-    </React.Fragment>);
+  return (
+    <React.Fragment>
+      <Typography
+        variant={"h2"}
+        align={"center"}
+        className={classes.categoryHeaders}
+      >
+        {title}
+      </Typography>
+      <GridContainer
+        spacing={5}
+        alignItems={"flex-end"}
+        justifyContent={"center"}
+      >
+        {tierCategory.membershipTiers.map((tier) => (
+          <GridItem key={tier.title} xs={12} sm={12} md={4}>
+            <Card className={classes.cards}>
+              <CardHeader
+                title={tier.title}
+                titleTypographyProps={{ align: "center" }}
+                subheaderTypographyProps={{ align: "center" }}
+                action={tier.title === "Standard" ? <StarIcon /> : null}
+                className={classes.cardHeader}
+              />
+              <CardContent>
+                <div className={classes.cardPricing}>
+                  <Typography component="h2" variant="h3" color="textPrimary">
+                    ${tier.price}
+                  </Typography>
+                  <Typography variant="h6" color="textSecondary">
+                    /{tier.term}
+                  </Typography>
+                </div>
+                <ul>
+                  {tier.description.map((line) => (
+                    <Typography
+                      component="li"
+                      variant="subtitle1"
+                      align="left"
+                      key={line}
+                    >
+                      <Divider />
+                      {line}
+                      <Divider />
+                    </Typography>
+                  ))}
+                </ul>
+              </CardContent>
+              <CardActions>
+                {tier.disableMembership ? (
+                  <Button
+                    fullWidth={maxWidth !== "sm"}
+                    variant={tier.buttonVariant}
+                    color="primary"
+                    href={"/contact"}
+                    className={classNames(
+                      tier.buttonVariant === "contained"
+                        ? classes.featuredPriceButton
+                        : classes.priceButton,
+                    )}
+                  >
+                    Email us to be placed on our Waitlist
+                  </Button>
+                ) : (
+                  <Button
+                    fullWidth={maxWidth !== "sm"}
+                    variant={tier.buttonVariant}
+                    color="primary"
+                    href={tier.inputValue}
+                    className={classNames(
+                      tier.buttonVariant === "contained"
+                        ? classes.featuredPriceButton
+                        : classes.priceButton,
+                    )}
+                  >
+                    {tier.buttonText}
+                  </Button>
+                )}
+              </CardActions>
+            </Card>
+          </GridItem>
+        ))}
+      </GridContainer>
+    </React.Fragment>
+  );
 }
--- src/pages/memberships.js
+++ src/pages/memberships.js
@@ -2,138 +2,172 @@ import React from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // @material-ui/core components
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 // core components
-import {Hidden, Typography} from "@material-ui/core";
+import { Hidden, Typography } from "@material-ui/core";
 // Sections for this page
 import Head from "next/head";
 import Pricing from "../components/Pricing/Pricing";
 import Layout from "../pages-sections/Page-Sections/Layout";
-import {brandFont, clp_exclaim, dividerBar, mainElement, playBrand, title,} from "../assets/jss/nextjs-material-kit";
+import {
+  brandFont,
+  clp_exclaim,
+  dividerBar,
+  mainElement,
+  playBrand,
+  title,
+} from "../assets/jss/nextjs-material-kit";
 import BrandedHeader from "../components/BrandedHeader/BrandedHeader";
-import {queryCMS} from "../utils/queryCMS";
+import { queryCMS } from "../utils/queryCMS";
 import Container from "@material-ui/core/Container";
 import gql from "graphql-tag";
-import {ShowPreview} from "../components/ShowPreview/showPreview";
+import { ShowPreview } from "../components/ShowPreview/showPreview";
 
 const style = (theme) => ({
-    whiteContainer:{
-        backgroundColor: "#fff"
-    },
-    headerContainer2: {
-        ...mainElement,
-        padding: "1rem",
-        backgroundColor: theme.palette.primary.main,
-        marginTop: "0"
-    },
-    verticalCones: {
-        height:"20rem",
-        marginTop: "1rem",
-        marginBottom: "1rem"
-    },
-    horizontalCones: {
-        width: "10rem"
-    },
-    whiteHeader: {
-        ...brandFont,
-        color: theme.palette.primary.contrastText,
-        textShadow: "1px 1px 1px #000000",
-    },
-    raisedDivider: {
-        ...dividerBar,
-        ...mainElement,
-        marginTop: "0"
-    },
-    textBody: {
-        maxWidth: "50vw",
-        margin: "auto",
-        paddingBottom: "2.5rem"
-    },
-    mainElement,
-    title,
-    brandFont,
-    clp_exclaim,
-    playBrand,
-    dividerBar
+  whiteContainer: {
+    backgroundColor: "#fff",
+  },
+  headerContainer2: {
+    ...mainElement,
+    padding: "1rem",
+    backgroundColor: theme.palette.primary.main,
+    marginTop: "0",
+  },
+  verticalCones: {
+    height: "20rem",
+    marginTop: "1rem",
+    marginBottom: "1rem",
+  },
+  horizontalCones: {
+    width: "10rem",
+  },
+  whiteHeader: {
+    ...brandFont,
+    color: theme.palette.primary.contrastText,
+    textShadow: "1px 1px 1px #000000",
+  },
+  raisedDivider: {
+    ...dividerBar,
+    ...mainElement,
+    marginTop: "0",
+  },
+  textBody: {
+    maxWidth: "50vw",
+    margin: "auto",
+    paddingBottom: "2.5rem",
+  },
+  mainElement,
+  title,
+  brandFont,
+  clp_exclaim,
+  playBrand,
+  dividerBar,
 });
 
 const useStyles = makeStyles(style);
 
 const membershipsQuery = gql`
-    {
-        membershipCategories{
-            title
-            membershipTiers{
-                title
-                price
-                description
-                buttonText
-                buttonVariant
-                inputValue
-                term
-            }
-        }
-    }`
+  {
+    membershipCategories {
+      title
+      membershipTiers {
+        title
+        price
+        description
+        buttonText
+        buttonVariant
+        inputValue
+        term
+        disableMembership
+      }
+    }
+  }
+`;
 
 export async function getStaticProps(context) {
+  const preview = context.preview ? context.preview : null;
+  const prodToken = process.env.NEXT_PUBLIC_GRAPHCMS_WEBCLIENT_API_TOKEN;
+  const token = preview
+    ? context.previewData.token +
+      process.env.NEXT_PUBLIC_GRAPH_CMS_PREVIEW_TOKEN_CLIENT
+    : prodToken;
+  const endPoint = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-    const preview = context.preview ? context.preview : null;
-    const prodToken = process.env.NEXT_PUBLIC_GRAPHCMS_WEBCLIENT_API_TOKEN;
-    const token = preview ? (context.previewData.token + process.env.NEXT_PUBLIC_GRAPH_CMS_PREVIEW_TOKEN_CLIENT) : prodToken;
-    const endPoint = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+  const membershipsQueryResult = await queryCMS(
+    membershipsQuery,
+    token,
+    endPoint,
+  );
 
-    const membershipsQueryResult = await queryCMS(membershipsQuery, token, endPoint);
-
-    return {
-        props: {membershipsQueryResult, preview}, // will be passed to the page component as props
-    }
+  return {
+    props: { membershipsQueryResult, preview }, // will be passed to the page component as props
+  };
 }
 
 export default function MembershipPage(props) {
-    const classes = useStyles();
-    const { membershipsQueryResult, preview } = props
+  const classes = useStyles();
+  const { membershipsQueryResult, preview } = props;
 
-    const priceLists = (categoriesList, maxWidth) => {
-        let result = [];
-        categoriesList.membershipCategories.map((category) => {
-            result.push(<Pricing tierCategory={category} title={category.title} key={category.title} maxWidth={maxWidth}/>)
-        })
-        return result;
-    };
+  const priceLists = (categoriesList, maxWidth) => {
+    let result = [];
+    categoriesList.membershipCategories.map((category) => {
+      result.push(
+        <Pricing
+          tierCategory={category}
+          title={category.title}
+          key={category.title}
+          maxWidth={maxWidth}
+        />,
+      );
+    });
+    return result;
+  };
 
-    return (
-        <div>
-                <Head>
-                    <title>Memberships • Bozeman Community Kiln</title>
-                    <link rel="canonical" href="https://bckstudio.com/memberships/"/>
-                    <meta name="description"
-                          content="Come Create with us. Pottery membership options to meet your skill and need to work with clay. Use our professional level pottery tools and equipment and earn discounts on classes and more."/>
-                </Head>
-            <div>
-                <ShowPreview preview={preview} page={"memberships"}/>
-                <Layout>
-                    <BrandedHeader>
-                        <Typography variant={"h1"} align={"center"} className={classNames(classes.brandFont)} gutterBottom>
-                            CREATE.
-                        </Typography>
-                        <div className={classes.textBody}>
-                            <Typography variant={"body1"} component={"p"} align={"center"}>
-                                Thank you for your interest in joining our clay family. Memberships keep us open and allow members access to the studio and equipment up to 24/7. During regular business hours, Ashleah and Heather are available to meet the individualized needs of our members. Read the membership descriptions below and find the one that's right for you.
-                            </Typography>
-                        </div>
-                    </BrandedHeader>
-                    <Hidden smDown>
-                        <Container component="main" maxWidth={"lg"}>
-                            {priceLists(membershipsQueryResult, "lg")}
-                        </Container>
-                    </Hidden>
-                    <Hidden mdUp>
-                        <Container component="main" maxWidth={"sm"}>
-                            {priceLists(membershipsQueryResult, "sm")}
-                        </Container>
-                    </Hidden>
-                </Layout>
+  return (
+    <div>
+      <Head>
+        <title>Memberships • Bozeman Community Kiln</title>
+        <link rel="canonical" href="https://bckstudio.com/memberships/" />
+        <meta
+          name="description"
+          content="Come Create with us. Pottery membership options to meet your skill and need to work with clay. Use our professional level pottery tools and equipment and earn discounts on classes and more."
+        />
+      </Head>
+      <div>
+        <ShowPreview preview={preview} page={"memberships"} />
+        <Layout>
+          <BrandedHeader>
+            <Typography
+              variant={"h1"}
+              align={"center"}
+              className={classNames(classes.brandFont)}
+              gutterBottom
+            >
+              CREATE.
+            </Typography>
+            <div className={classes.textBody}>
+              <Typography variant={"body1"} component={"p"} align={"center"}>
+                Thank you for your interest in joining our clay family.
+                Memberships keep us open and allow members access to the studio
+                and equipment up to 24/7. During regular business hours, Ashleah
+                and Heather are available to meet the individualized needs of
+                our members. Read the membership descriptions below and find the
+                one that's right for you.
+              </Typography>
             </div>
-        </div>
-    );
+          </BrandedHeader>
+          <Hidden smDown>
+            <Container component="main" maxWidth={"lg"}>
+              {priceLists(membershipsQueryResult, "lg")}
+            </Container>
+          </Hidden>
+          <Hidden mdUp>
+            <Container component="main" maxWidth={"sm"}>
+              {priceLists(membershipsQueryResult, "sm")}
+            </Container>
+          </Hidden>
+        </Layout>
+      </div>
+    </div>
+  );
 }
